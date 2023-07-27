2baf7b0380f0993c3652f4f797ba9349a635489d
manaboy-07 <manassehoruebor@gmail.com>
2023-07-27 23:10:05 +0100

    top buttons addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
-
+import TopButtons from './components/TopButtons';
+// import UilReact from '@iconscout/react-unicons/icons/uil-react'
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-grade-400">
+      
+      <TopButtons/>
     </div>
   );
 }
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TopButtons() {
+    const cities = [
+        {
+            id:1,
+            title: 'London'
+        },
+    ]
+  return (
+    <div>TopButtons</div>
+  )
+}
+
+export default TopButtons
