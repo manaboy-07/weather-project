f3fe640994f2c40e32841b4965ddf96df1f54cd1
manaboy-07 <manassehoruebor@gmail.com>
2023-07-28 00:18:34 +0100

    stiil going at it

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 
 import "./App.css";
 import Inputs from "./components/Inputs";
+import TempDetails from "./components/TempDetails";
 import TimeAndLoacation from "./components/TimeAndLoacation";
 import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-grade-400'>
+    <div className=' rounded-md mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-grade-400'>
       <TopButtons />
       <Inputs />
       <TimeAndLoacation />
+      <TempDetails/>
     </div>
   );
 }
--- src/components/TempDetails.jsx
+++ src/components/TempDetails.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+/** @format */
 
+import React from "react";
+import {
+  UilArrowDown,
+  UilArrowUp,
+  UilTemperature,
+  UilTear,
+  UilSunset,
+  UilWind,
+  UilSun,
+} from "@iconscout/react-unicons";
 function TempDetails() {
   return (
     <div>
-       <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
-         <p>Cloudy</p>
-       </div>
-       <div className="flex flex-row items-center justify-between py-3 text-white">
-           <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" className='w-20'/>
-           <p className="text-5xl">34°</p>
-           <div className='space-y-2 flex flex-col '>
-            <div className="flex font-light text-sm items-center justify-center">
-                
-            </div>
-           </div>
-       </div>
+      <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
+        <p className="text-white">Cloudy</p>
+      </div>
+      <div className='flex flex-row items-center justify-between py-3 text-white'>
+        <img
+          src='http://openweathermap.org/img/wn/01d@2x.png'
+          alt=''
+          className='w-20'
+        />
+        <p className='text-5xl'>34°</p>
+        <div className='space-y-2 flex flex-col '>
+          <div className='flex font-light text-sm items-center justify-center'>
+            <UilTemperature size={18} className='mr-1'/>
+            Real fell
+            <span className="font-medium ml-1">34°</span>
+          </div>
+          <div className='flex font-light text-sm items-center justify-center'>
+            <UilTear size={18} className='mr-1'/>
+            Humidity
+            <span className="font-medium ml-1">65%</span>
+          </div>
+          <div className='flex font-light text-sm items-center justify-center'>
+            <UilTemperature size={18} className='mr-1'/>
+            Wind 
+            <span className="font-medium ml-1">3 km/h</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TempDetails
+export default TempDetails;
