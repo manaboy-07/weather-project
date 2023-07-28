c809cf7ef57bc86f06a2f58a5c3e8d3a4a0518a3
manaboy-07 <manassehoruebor@gmail.com>
2023-07-28 01:37:56 +0100

    working on weather forecast

--- src/components/TempDetails.jsx
+++ src/components/TempDetails.jsx
@@ -14,7 +14,7 @@ function TempDetails() {
   return (
     <div>
       <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
-        <p className="text-white">Cloudy</p>
+        <p className='text-white'>Cloudy</p>
       </div>
       <div className='flex flex-row items-center justify-between py-3 text-white'>
         <img
@@ -25,21 +25,47 @@ function TempDetails() {
         <p className='text-5xl'>34°</p>
         <div className='space-y-2 flex flex-col '>
           <div className='flex font-light text-sm items-center justify-center'>
-            <UilTemperature size={18} className='mr-1'/>
+            <UilTemperature size={18} className='mr-1' />
             Real fell
-            <span className="font-medium ml-1">34°</span>
+            <span className='font-medium ml-1'>34°</span>
           </div>
           <div className='flex font-light text-sm items-center justify-center'>
-            <UilTear size={18} className='mr-1'/>
+            <UilTear size={18} className='mr-1' />
             Humidity
-            <span className="font-medium ml-1">65%</span>
+            <span className='font-medium ml-1'>65%</span>
           </div>
           <div className='flex font-light text-sm items-center justify-center'>
-            <UilTemperature size={18} className='mr-1'/>
-            Wind 
-            <span className="font-medium ml-1">3 km/h</span>
+            <UilTemperature size={18} className='mr-1' />
+            Wind
+            <span className='font-medium ml-1'>3 km/h</span>
           </div>
         </div>
+      </div>
+
+      <div className='flex w-full flex-row text-white items-center justify-center space-x-2 text-sm py-3'>
+        <UilSun />
+        <p className='font-light'>
+          Rise : <span className="font-medium">06:45 AM</span>
+        </p>
+        <p className='font-light'>|</p>
+
+        <UilSunset />
+        <p className='font-light'>
+          Set : <span className="font-medium">07:45 PM</span>
+        </p>
+        <p className='font-light'>|</p>
+
+        <UilArrowUp />
+        <p className='font-light'>
+          High : <span className="font-medium">45°</span>
+        </p>
+        <p className='font-light'>|</p>
+
+        <UilArrowDown/>
+        <p className='font-light'>
+          Low : <span className="font-medium">40°</span>
+        </p>
+        
       </div>
     </div>
   );
--- src/components/TimeAndLoacation.jsx
+++ src/components/TimeAndLoacation.jsx
@@ -5,7 +5,7 @@ function TimeAndLoacation() {
     <div>
         <div className="flex items-center justify-center my-6">
             <p className="text-white text-xl font-extralight">
-                Tuesdat, 31 May 2334 | Local time : 12:46 PM
+                Tuesday, 31 May 2334 | Local time : 12:46 PM
             </p>
         </div>
         <div className="flex items-center justify-center my-3">
