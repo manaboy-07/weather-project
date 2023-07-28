3cd02f4f58811737f7f7fa6977e23fcb830c7f85
manaboy-07 <manassehoruebor@gmail.com>
2023-07-28 20:06:10 +0100

    working on hourly forecast

---
+++ src/components/Forecast.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Forecast() {
+  return (
+    <div>
+        <div className="flex item-center justify-start mt-6">
+            <p className="text-white font-medium uppercase">
+                Hourly Forecast
+            </p>
+        </div>
+        <hr className="my-2" />
+    </div>
+  )
+}
+
+export default Forecast
